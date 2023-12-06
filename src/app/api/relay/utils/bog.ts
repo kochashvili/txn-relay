ab690ed2c7a88c5bd2696348fafcf4466fa6413f
Jaba Kochashvili <43106084+kochashvili@users.noreply.github.com>
2023-12-06 17:38:47 +0400

    save file

--- src/app/api/relay/utils/bog.ts
+++ src/app/api/relay/utils/bog.ts
@@ -2,7 +2,7 @@ import { GoogleSpreadsheet } from "google-spreadsheet";
 import { jwtFromEnv } from "./jwt";
 
 export const saveBogTxn = async (message: string) => {
-  const result = parseBogMessage(message);
+  const result = parseBogTxn(message);
 
   const doc = new GoogleSpreadsheet(
     process.env.GOOGLE_SPREADSHEET_ID!,
@@ -14,7 +14,7 @@ export const saveBogTxn = async (message: string) => {
   await BogSheet.addRow(result);
 };
 
-const parseBogMessage = (message: string) => {
+const parseBogTxn = (message: string) => {
   const regex =
     /Purchase: ([A-Z]+) ([\d\.,]+) Card: \*\*\*\* (\d{4}) (.*?) Remaining balance: ([A-Z]+) ([\d\.,]+) You will get: ([\d\.,]+) MR Available MR points: ([\d\.,]+) 150 MR = 1 Gel (\d{2}\.\d{2}\.\d{4})/;
   const match = message.match(regex);
