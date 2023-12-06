046b22b7925663f4e7bab1de7f3e44a9ff23c532
Jaba Kochashvili <43106084+kochashvili@users.noreply.github.com>
2023-12-06 14:59:35 +0400

    save file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p className="flex justify-center border-b border-gray-300 bg-gradient-to-b rounded-xl border bg-gray-200 p-4 dark:bg-zinc-800/30" from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit">
+      <p className="flex justify-center border-b border-gray-300 bg-gradient-to-b rounded-xl border bg-gray-200 p-4  from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit">
         Get started by editing&nbsp;
         <code className="font-mono font-bold">src/app/page.tsx</code>
       </p>
