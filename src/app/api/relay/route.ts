d1dd915c8f1e203fcece95b70c6db5104a0be4ba
Jaba Kochashvili <43106084+kochashvili@users.noreply.github.com>
2023-12-06 16:11:51 +0400

    save file

--- src/app/api/relay/route.ts
+++ src/app/api/relay/route.ts
@@ -8,6 +8,10 @@ interface Data {
 export async function POST(request: Request): Promise<NextResponse> {
   const data: Data = await request.json();
 
+  if (data.sender === "TBC") saveTbcTxn(data);
+  if (data.sender === "BOG") saveBogTxn(data);
+  if (data.sender === "Liberty") saveLibertyTxn(data);
+
   console.log(data);
 
   return NextResponse.json(data);
