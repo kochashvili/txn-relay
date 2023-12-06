cd30b69d3ffbb8da8a3a0e818d3f5646ee8f3710
Jaba Kochashvili <43106084+kochashvili@users.noreply.github.com>
2023-12-06 15:02:33 +0400

    save file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,10 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <p className="flex justify-center border-b border-gray-300 bg-gradient-to-b rounded-xl border bg-gray-200 p-4  from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit">
-        <div>div className="w-3 h-3 rounded-full bg-green-600 animate-pulse" />
-        Relay is active.
-          </div><
+        <div>
+          <div className="w-3 h-3 rounded-full bg-green-600 animate-pulse" />
+          Relay is active.
+        </div>
       </p>
     </main>
   );
