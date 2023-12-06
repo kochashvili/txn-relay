c322be0e01f178e6390207f54ca56d5e3c5f1806
Jaba Kochashvili <43106084+kochashvili@users.noreply.github.com>
2023-12-06 17:43:34 +0400

    save file

--- src/app/api/relay/utils/tbc.ts
+++ src/app/api/relay/utils/tbc.ts
@@ -20,6 +20,8 @@ const parseTbcTxn = (message: string) => {
   const match = message.match(regex);
   const result: Record<string, string | number> = {};
 
+  console.log(match);
+
   if (match) {
     result["Type"] = "purchase";
     result["Currency"] = match[1];
