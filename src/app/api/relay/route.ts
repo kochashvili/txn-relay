5e977a6cfc2fa919ee5437b4997a9561bc3c6a30
Jaba Kochashvili <43106084+kochashvili@users.noreply.github.com>
2023-12-06 16:07:38 +0400

    save file

--- src/app/api/relay/route.ts
+++ src/app/api/relay/route.ts
@@ -1,5 +1,9 @@
 import { NextResponse } from "next/server";
 
+interface Data {
+  sender: "TBC" | "BOG" | "Liberty";
+}
+
 export async function POST(request: Request): Promise<NextResponse> {
   const data = await request.json();
 
