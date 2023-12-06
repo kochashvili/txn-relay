a8c86c68e7e3750750ef9ba7ccfdcee1d79fff74
Jaba Kochashvili <43106084+kochashvili@users.noreply.github.com>
2023-12-06 18:03:23 +0400

    save file

--- src/app/api/relay/utils/liberty.ts
+++ src/app/api/relay/utils/liberty.ts
@@ -16,7 +16,7 @@ export const saveLibertyTxn = async (message: string) => {
 
 const parseLibertyTxn = (message: string) => {
   const regex =
-    /საბარათე ოპერაცია: ([A-Z]+) ([\d\.,]+)\s+ქეშბექი: ([A-Z]+) ([\d\.,]+) (.*?) cashback: ([A-Z]+) ([\d\.,]+)\s+ბარათი: (.*?) \/ \.\.\.\.(\d{4})\s+(.*?) თარიღი: (\d{2}\/\d{2}\/\d{4}) ნაშთი: ([A-Z]+) ([\d\.,]+)/;
+    /საბარათე ოპერაცია: ([A-Z]+) ([\d\.,]+)\s+  ქეშბექი: ([A-Z]+)  ([\d\.,]+) (.*?) cashback: ([A-Z]+) ([\d\.,]+)\s+ბარათი: (.*?) \/ \.\.\.\.(\d{4})\s+(.*?)  თარიღი: (\d{2}\/\d{2}\/\d{4})  ნაშთი: ([A-Z]+) ([\d\.,]+)/;
   const match = message.match(regex);
   const result: Record<string, string | number> = {};
 
