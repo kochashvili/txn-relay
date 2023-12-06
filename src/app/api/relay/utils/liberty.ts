8bf4de6dd630cb24d05946df5838db33629d6ba8
Jaba Kochashvili <43106084+kochashvili@users.noreply.github.com>
2023-12-06 18:08:12 +0400

    save file

--- src/app/api/relay/utils/liberty.ts
+++ src/app/api/relay/utils/liberty.ts
@@ -24,13 +24,14 @@ const parseLibertyTxn = (message: string) => {
 
   if (match) {
     result["Type"] = "purchase";
-    result["Currency"] = match[2];
-    result["Amount"] = +match[1].replace(",", "");
-    result["Card"] = match[3] + " " + match[4];
-    result["Merchant"] = match[5].trim();
-    result["Balance Currency"] = match[9];
-    result["Remaining Balance"] = +match[8].replace(",", "");
-    result["Date"] = match[6];
+    result["Currency"] = match[1];
+    result["Amount"] = +match[2].replace(",", "");
+    result["Card"] = match[8] + " " + match[9];
+    result["Merchant"] = match[10].trim();
+    result["Balance Currency"] = match[12];
+    result["Remaining Balance"] = +match[13].replace(",", "");
+    result["Date"] = match[11];
+    result["Cashback"] = +match[4] + +match[7];
   }
 
   return result;
