6581bb59978b7de98b28c95aeef0de91742c32e2
Jaba Kochashvili <43106084+kochashvili@users.noreply.github.com>
2023-12-06 15:02:14 +0400

    save file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <p className="flex justify-center border-b border-gray-300 bg-gradient-to-b rounded-xl border bg-gray-200 p-4  from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit">
         <div className="w-3 h-3 rounded-full bg-green-600 animate-pulse" />{" "}
-        Relay is active. Get started by editing&nbsp;
-        <code className="font-mono font-bold">src/app/page.tsx</code>
+        Relay is active.
       </p>
     </main>
   );
