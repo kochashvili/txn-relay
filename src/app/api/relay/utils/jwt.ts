95e9d123564c0cbb8641cbd5fd429f8b73640411
Jaba Kochashvili <43106084+kochashvili@users.noreply.github.com>
2023-12-06 17:13:25 +0400

    save file

--- src/app/api/relay/utils/jwt.ts
+++ src/app/api/relay/utils/jwt.ts
@@ -1,5 +1,10 @@
 import { JWT } from "google-auth-library";
 
+const SCOPES = [
+  "https://www.googleapis.com/auth/spreadsheets",
+  "https://www.googleapis.com/auth/drive.file",
+];
+
 const jwtFromEnv = new JWT({
   email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
   key: process.env.GOOGLE_PRIVATE_KEY,
