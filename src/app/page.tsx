1516c70237d053f0b94a7232bc91bc384b3d0bc4
Jaba Kochashvili <43106084+kochashvili@users.noreply.github.com>
2023-12-06 15:04:28 +0400

    save file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <p className="flex justify-center border-b border-gray-300 bg-gradient-to-b rounded-xl border bg-gray-200 p-4  from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit">
+      <p className="flex justify-center border-b border-gray-300 bg-gradient-to-b rounded-xl border bg-gray-200 px-6 py-4  from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit">
         <span className="flex gap-2 items-center">
           <span className="w-3 h-3 rounded-full bg-green-600 animate-pulse" />
           Relay is active.
