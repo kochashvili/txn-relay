4afc66d1f854a2159eb5ced3ffc71e7b9dc79354
Jaba Kochashvili <43106084+kochashvili@users.noreply.github.com>
2023-12-06 17:38:55 +0400

    save file

--- src/app/api/relay/utils/tbc.ts
+++ src/app/api/relay/utils/tbc.ts
@@ -15,5 +15,24 @@ export const saveTbcTxn = async (message: string) => {
 };
 
 const parseTbcTxn = (message: string) => {
+  const regex =
+    /Purchase: ([A-Z]+) ([\d\.,]+) Card: \*\*\*\* (\d{4}) (.*?) Remaining balance: ([A-Z]+) ([\d\.,]+) You will get: ([\d\.,]+) MR Available MR points: ([\d\.,]+) 150 MR = 1 Gel (\d{2}\.\d{2}\.\d{4})/;
+  const match = message.match(regex);
+  const result: Record<string, string | number> = {};
+
+  if (match) {
+    result["Type"] = "purchase";
+    result["Currency"] = match[1];
+    result["Amount"] = +match[2].replace(",", "");
+    result["Card"] = match[3];
+    result["Merchant"] = match[4].trim();
+    result["Balance Currency"] = match[5];
+    result["Remaining Balance"] = +match[6].replace(",", "");
+    result["Collected MR points"] = +match[7].replace(",", "");
+    result["MR Points Balance"] = +match[8].replace(",", "");
+    result["Date"] = match[9];
+  }
+
+  return result;
   return {};
 };
