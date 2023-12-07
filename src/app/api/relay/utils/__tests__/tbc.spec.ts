7b8fa9a9a3481942c4eb977061f8f65bebc72b45
Jaba Kochashvili <43106084+kochashvili@users.noreply.github.com>
2023-12-07 15:44:41 +0400

    save file

--- src/app/api/relay/utils/__tests__/tbc.spec.ts
+++ src/app/api/relay/utils/__tests__/tbc.spec.ts
@@ -1,8 +1,3 @@
 const txn_messages = [
-    "0.99 GEL
-    VISA ERTGULI PLATINUM (***3224)
-    CARREFOUR(SABA)
-    19/11/2023 20:38:46
-    ნაშთი: 4892.65 GEL",
-
+  "0.99 GEL VISA ERTGULI PLATINUM (***3224) CARREFOUR(SABA) 19/11/2023 20:38:46 ნაშთი: 4892.65 GEL",
 ];
