19fca2281e153d95bc3eb01021d1a40152c30187
Jaba Kochashvili <43106084+kochashvili@users.noreply.github.com>
2023-12-07 15:55:36 +0400

    save file

--- src/app/api/relay/utils/jwt.ts
+++ src/app/api/relay/utils/jwt.ts
@@ -4,6 +4,6 @@ const scopes = ["https://www.googleapis.com/auth/spreadsheets"];
 
 const { email, key } = JSON.parse(process.env.GOOGLE_AUTH!);
 
-console.log({ email, key });
+console.log({ email, key, str: process.env.GOOGLE_AUTH });
 
 export const jwtFromEnv = new JWT({ email, key, scopes });
